63cc0f88df795231ba267f2c56a549bfaf0add8e
ddmkim94 <ddmkim94@gmail.com>
2022-07-17 20:48:22 +0900

    명언 목록

--- src/main/java/com/ll/ssg/App.java
+++ src/main/java/com/ll/ssg/App.java
@@ -1,14 +1,18 @@
 package com.ll.ssg;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner sc;
+    private List<WiseSaying> wiseSayingList;
 
     // 주입! -> DI!
     public App(Scanner sc) {
         this.sc = sc;
+        wiseSayingList = new ArrayList<>();
     }
 
     public void run() {
@@ -22,6 +26,15 @@ public class App {
             String command = sc.nextLine().trim();
 
             switch (command) {
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+                    for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying = wiseSayingList.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getContent(), wiseSaying.getAuthor());
+                    }
+
+                    break;
                 case "등록":
                     System.out.print("명언 : ");
                     String content = sc.nextLine();
@@ -29,6 +42,8 @@ public class App {
                     String author = sc.nextLine();
 
                     int id = ++wiseSayingLastId;
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayingList.add(wiseSaying);
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
                 case "종료":
---
+++ src/main/java/com/ll/ssg/WiseSaying.java
@@ -0,0 +1,20 @@
+package com.ll.ssg;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+public class WiseSaying {
+
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
